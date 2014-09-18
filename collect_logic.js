d0155aab4c21c2e05bb5fd76824a577f59854124
Ben Mehne <bmehne@gmail.com>
2014-09-17 21:42:00 -0700

    Added initial testing files

--- collect_logic.js
+++ collect_logic.js
@@ -1,15 +1,17 @@
 
 function onButtonPress(clicked) {
-    recordTree(getSpecies(), clicked.product_type, clicked.number_of_sticks, getPlotNumber());
+	alert('called on button press');
+	console.log('called on button press wik', clicked);
+	recordTree(getSpecies(), clicked.getAttribute('product_type'), clicked.getAttribute('number_of_sticks'), getPlotNumber());
 }
 
 var data= [];
 function recordTree(tree_species, product_type, number_of_sticks, plot_number) {
 // add some storage stuff
 data.push({
-    'tree_species': tree_species, 
-    'product_type': product_type, 
-    'number_of_sticks': number_of_sticks, 
+    'tree_species': tree_species,
+    'product_type': product_type,
+    'number_of_sticks': number_of_sticks,
     'plot_number': plot_number
     });
 }
@@ -17,7 +19,7 @@ data.push({
 function getStringCountOfSpecies(product_type, number_of_sticks){
     var map_of_species_to_counts = {};
     for(var i = 0; i < data.length; ++i){
-        if (data[i].product_type == product_type && 
+        if (data[i].product_type == product_type &&
         data[i].number_of_sticks == number_of_sticks){
         // TODO: worry about initial case (? +1 = what?)
             var old_count = map_of_species_to_counts[data[i].tree_species];
@@ -46,12 +48,20 @@ function toCSV(){
 var MAX_NUMBER_OF_STICKS = 8;
 var PRODUCT_TYPES = ['sawlogs', 'pulpsticks'];
 function generateButtons(){
-    var output_html = "";
+    var output_html = "<table>";
     for(var i = 0; i < MAX_NUMBER_OF_STICKS; ++i){
         output_html += "<tr>";
         for(var j = 0; j < PRODUCT_TYPES.length; ++j) {
-            output_html += '<td onclick="onButtonPress(this)" number_of_sticks="' + i + '" product_type="' + PRODUCT_TYPES[j] +'"> ' + getStringCountOfSpecies(PRODUCT_TYPE[j], i, getPlotNumber()) + '</td>';
+            output_html += '<td onclick="onButtonPress(this)" number_of_sticks="' + i + '" product_type="' + PRODUCT_TYPES[j] +'"> ' + getStringCountOfSpecies(PRODUCT_TYPES[j], i, getPlotNumber()) + '</td>';
         }
         output_html += "</tr>\n";
     }
+	 output_html += "</html>";
+	 return output_html;
+}
+
+function fillInTable(id) {
+	alert('called fillInTable');
+	var div = document.getElementById(id);
+	div.innerHTML = generateButtons();
 }
